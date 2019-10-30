b87dc7a8a25b28855ebd45064d40a5a06b99cfef
himaniyadav <30757425+himaniyadav@users.noreply.github.com>
2019-10-30 12:59:18 -0400

    Create puns.py

---
+++ puns.py
@@ -0,0 +1,14 @@
+print("Theme Options:")
+print("Music, People, Literature, Science, Math, School, Food, Countries, Yo Mama")
+Music =["Pop and rap","Orchestra","Viola","Band"]
+People=["Political","Famous","Historical"]
+Literature=["Historical","Popular"]
+Science=["Chemistry","Anatomy","Physics","Seasons","Plants","Animals","Planets",]
+Math=["Shapes","Numbers","Algebra","Equations","Calculus"]
+School=["High School","Private School","Public School","Middle School","Elementary School"]
+Food=["Desserts","Dinner","Lunch","Breakfast","Junk Food"]
+Countries=["CANADA","United States","Urban Countries"]
+print("Enter a theme ")
+print("Enter a subtheme")
+theme = input()
+subtheme=input()
