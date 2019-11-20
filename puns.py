bfaa8c58ef33230b31864c7ef1ff5c511fd502bc
maryannw <57181948+maryannw@users.noreply.github.com>
2019-11-20 09:15:28 -0500

    Update puns.py

--- puns.py
+++ puns.py
@@ -3,16 +3,53 @@ import random
 print("Theme Options:")
 print("Music, People, Literature, Science, Math, School, Food, Countries, Yo Mama")
 
-Music =["Pop and rap","Orchestra","Viola","Band"]
-People=["Political","Famous","Historical"]
+Music =["PopRap","Orchestra","Viola","Country"]
+People=["Political","Famous"]
 Literature=["Historical","Popular"]
-Science=["Chemistry","Anatomy","Physics","Seasons","Plants","Animals","Planets",]
+Science=["Seasons","Plants","Animals","Planets",]
 Math=["Shapes","Numbers","Algebra","Equations","Calculus"]
-School=["High School","Private School","Public School","Middle School","Elementary School"]
+School=["High","Private","Public","Elementary"]
 Food=["Desserts","Dinner","Lunch","Breakfast","Junk Food"]
-Countries=["CANADA","United States","Urban Countries"]
+Countries=["United States","Canada"]
+YoMama=["Jokes"]
+
+PopRap = ["What did Jay-Z call his wife before they got married?\nFeyonce.","How do you wake up Lady Gaga?\nYou poker face.","What's a rap fans favorite candy?\nEminem."]
+Orchestra=[" Why couldn’t the string quartet find their composer?\nHe was Haydn.","Are you calling me a lyre?!","It'll just take a minuet."]
+Viola=["Why are viola jokes so short?\nSo that violinists can understand them.","What's the difference between a viola and a vacuum cleaner?\nA vacuum cleaner has to be plugged in before it sucks.","What's the difference between a pizza and a violist?\nA pizza can feed a family of four."]
+Country=["Surprisingly John Deere wasn’t a country music fan.\nHe preferred Mowtown.","Music can take you places.\nToday for example, they put country music on and I went to the next bar.","Twenty years ago we had Johnny Cash, Bob Hope and Steve Jobs.\nNow we have no Cash, no Hope and no Jobs. Please don’t let Kevin Bacon die."]
 
 Political=["Pun1", "Pun2", "Pun3"]
+Famous=["Pun1", "Pun2", "Pun3"]
+
+Historical=["Pun1", "Pun2", "Pun3"]
+Popular=["Pun1", "Pun2", "Pun3"]
+
+Seasons=["Pun1", "Pun2", "Pun3"]
+Plants=["Pun1", "Pun2", "Pun3"]
+Animals=["Pun1", "Pun2", "Pun3"]
+Planets=["Pun1", "Pun2", "Pun3"]
+
+Shapes=["Pun1", "Pun2", "Pun3"]
+Numbers=["Pun1", "Pun2", "Pun3"]
+Algebra=["Pun1", "Pun2", "Pun3"]
+Equations=["Pun1", "Pun2", "Pun3"]
+Calculus=["Pun1", "Pun2", "Pun3"]
+
+High=["Pun1", "Pun2", "Pun3"]
+Public=["Pun1", "Pun2", "Pun3"]
+Private=["Pun1", "Pun2", "Pun3"]
+Elementary=["Pun1", "Pun2", "Pun3"]
+
+Desserts=["Pun1", "Pun2", "Pun3"]
+Dinner=["Pun1", "Pun2", "Pun3"]
+Lunch=["Pun1", "Pun2", "Pun3"]
+Breakfast=["Pun1", "Pun2", "Pun3"]
+JunkFood=["Pun1", "Pun2", "Pun3"]
+
+UnitedStates=["Pun1", "Pun2", "Pun3"]
+Canada=["Pun1", "Pun2", "Pun3"]
+
+Jokes=["Pun1", "Pun2", "Pun3"]
 
 
 print("Enter a theme: ")
@@ -58,3 +95,11 @@ num = random.randint(0,2)
 
 if subtheme=="Political":
   print (Political[num])
+if subtheme=="Orchestra":
+  print (Orchestra[num])
+if subtheme=="PopRap":
+  print (PopRap[num])
+if subtheme=="Viola":
+  print (Viola[num])
+if subtheme=="Country":
+  print(Country[num])
